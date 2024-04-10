011735f5f5b5933fba361f7b2959c6812593554d
root <magma.mg@gmail.com>
2024-04-10 00:38:29 -0700

    lists1

--- lists1.c
+++ lists1.c
@@ -54,20 +54,24 @@ char **list_to_strings(list_t *head)
 	return (strs);
 }
 
-#include "shell.h"
 
 /**
- * list_len - determines length of linked list
+ * print_list - prints all elements of a list_t linked list
  * @h: pointer to first node
  *
  * Return: size of list
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
+		_puts(convert_number(h->num, 10, 0));
+		_putchar(':');
+		_putchar(' ');
+		_puts(h->str ? h->str : "(nil)");
+		_puts("\n");
 		h = h->next;
 		i++;
 	}
@@ -75,39 +79,25 @@ size_t list_len(const list_t *h)
 }
 
 /**
- * list_to_strings - returns an array of strings of the list->str
- * @head: pointer to first node
+ * node_starts_with - returns node whose string starts with prefix
+ * @node: pointer to list head
+ * @prefix: string to match
+ * @c: the next character after prefix to match
  *
- * Return: array of strings
+ * Return: match node or null
  */
-char **list_to_strings(list_t *head)
+list_t *node_starts_with(list_t *node, char *prefix, char c)
 {
-	list_t *node = head;
-	size_t i = list_len(head), j;
-	char **strs;
-	char *str;
+	char *p = NULL;
 
-	if (!head || !i)
-		return (NULL);
-	strs = malloc(sizeof(char *) * (i + 1));
-	if (!strs)
-		return (NULL);
-	for (i = 0; node; node = node->next, i++)
+	while (node)
 	{
-		str = malloc(_strlen(node->str) + 1);
-		if (!str)
-		{
-			for (j = 0; j < i; j++)
-				free(strs[j]);
-			free(strs);
-			return (NULL);
-		}
-
-		str = _strcpy(str, node->str);
-		strs[i] = str;
+		p = starts_with(node->str, prefix);
+		if (p && ((c == -1) || (*p == c)))
+			return (node);
+		node = node->next;
 	}
-	strs[i] = NULL;
-	return (strs);
+	return (NULL);
 }
 
 /**
